7dd4dfa2019f639c2eb3c3d0b489df6b84a2fc14
gradzi <gradzi@outbrain.com>
2019-01-15 01:21:31 +0200

    Add all files

---
+++ terraform/consul.tf
@@ -0,0 +1,75 @@
+# Create the user-data for the Consul server
+data "template_file" "consul_server" {
+  count    = "${var.servers}"
+  template = "${file("${path.module}/templates/consul.sh.tpl")}"
+
+  vars {
+    consul_version = "${var.consul_version}"
+    config = <<EOF
+     "node_name": "opsschool-server-${count.index+1}",
+     "server": true,
+     "bootstrap_expect": 3,
+     "ui": true,
+     "client_addr": "0.0.0.0"
+    EOF
+  }
+}
+
+# Create the user-data for the Consul agent
+data "template_file" "consul_client" {
+  count    = "${var.clients}"
+  template = "${file("${path.module}/templates/consul.sh.tpl")}"
+
+  vars {
+    consul_version = "${var.consul_version}"
+    config = <<EOF
+     "node_name": "opsschool-client-${count.index+1}",
+     "enable_script_checks": true,
+     "server": false
+    EOF
+  }
+}
+
+# Create the Consul cluster
+resource "aws_instance" "consul_server" {
+  count = "${var.servers}"
+
+  ami           = "ami-0653e888ec96eab9b"
+  instance_type = "t2.micro"
+  key_name      = "radzi"
+
+  iam_instance_profile   = "${aws_iam_instance_profile.consul-join.name}"
+  vpc_security_group_ids = ["${aws_security_group.opsschool_consul.id}"]
+
+  tags = {
+    Name = "opsschool-server-${count.index+1}"
+    consul_server = "true"
+  }
+
+  user_data = "${element(data.template_file.consul_server.*.rendered, count.index)}"
+}
+
+resource "aws_instance" "consul_client" {
+  count = "${var.clients}"
+
+  ami           = "ami-0653e888ec96eab9b"
+  instance_type = "t2.micro"
+  key_name      = "${var.key_name}"
+
+  iam_instance_profile   = "${aws_iam_instance_profile.consul-join.name}"
+  vpc_security_group_ids = ["${aws_security_group.opsschool_consul.id}"]
+
+  tags = {
+    Name = "opsschool-client-${count.index+1}"
+  }
+
+  user_data = "${element(data.template_file.consul_client.*.rendered, count.index)}"
+}
+
+output "servers" {
+  value = ["${aws_instance.consul_server.*.public_ip}"]
+}
+
+output "clients" {
+  value = ["${aws_instance.consul_client.*.public_ip}"]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "servers" {
+  description = "The number of consul servers."
+  default = 3
+}
+
+variable "clients" {
+  description = "The number of consul client instances"
+  default = 1
+}
+
+variable "consul_version" {
+  description = "The version of Consul to install (server and client)."
+  default     = "1.4.0"
+}
+
+variable "key_name" {
+  description = "name of ssh key to attach to hosts"
+}
